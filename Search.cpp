76ec44a6073ef1ce11a8518ce661e990eae1c585
Taidebug <nguyenthanhtaicva7a8@gmail.com>
2026-03-11 23:35:10 +0700

    code

--- Search.cpp
+++ Search.cpp
@@ -17,11 +17,11 @@ private:
     vector<string> word;
     unordered_map<string, string> dict;
     struct Node {
-        Node *child[256];
+        Node *child[26];
         int cnt;
         int exist;
         Node () {
-            for (int i = 0; i < 256; i++) child[i] = nullptr;
+            for (int i = 0; i < 26; i++) child[i] = nullptr;
             cnt = 0;
             exist = 0;
         }
@@ -35,35 +35,36 @@ private:
         return &node[cur++];
     }
 
-    void dfs(Node *p, string &cur) {
-        if (p->exist) {
-            word.push_back(cur);
-        }
-        for (int i = 0; i < 256; i++) {
-            if (p->child[i]) {
-                cur.push_back(char(i));
-                dfs(p->child[i], cur);
-                cur.pop_back();
-            }
-        }
-    }
-
-    void add(string s) {
-        Node *p = root;
-        for (char c: s) {
-            if (p->child[c] == nullptr) p->child[c] = newNode();
-            p = p->child[c];
-            p->cnt++;
-        }
-        p->exist++;
-    }
-
     string lower(string s) {
         for (char &c: s) {
             if ('A' <= c && c <= 'Z') c = c - 'A' + 'a';
         }
         return s;
     }
+
+    void add(string s) {
+        Node *p = root;
+        for (char c: s) {
+            if (p->child[c - 'a'] == nullptr) p->child[c - 'a'] = newNode();
+            p = p->child[c - 'a'];
+            p->cnt++;
+        }
+        p->exist++;
+    }
+
+    void dfs(Node *p, string &cur) {
+        if (p->exist) {
+            word.push_back(cur);
+        }
+        for (int i = 0; i < 26; i++) {
+            if (p->child[i]) {
+                cur.push_back(char(i + 'a'));
+                dfs(p->child[i], cur);
+                cur.pop_back();
+            }
+        }
+    }
+    
 public:
 
     void load_dictionary(string filename) {

894b9b520230a80063b14de80333fb1f266042b3
Taidebug <nguyenthanhtaicva7a8@gmail.com>
2026-03-23 22:27:07 +0700

    push file search

--- C++/Search.cpp
+++ C++/Search.cpp
@@ -12,112 +12,152 @@ using json = nlohmann::json;
 #define maxn 200007
 json dt;
 
-class SEARCH_DICTIONARY {
+struct WordData
+{
+    string definition;
+    string definition_vi;
+    bool in_roadmap;
+    vector<string> roadmap;
+    string term;
+};
+
+class SEARCH_DICTIONARY
+{
 private:
     vector<string> word;
-    unordered_map<string, string> dict;
-    unordered_map<string, string> term;
-    struct Node {
+    struct Node
+    {
         Node *child[256];
         int cnt;
         int exist;
-        Node () {
-            for (int i = 0; i < 256; i++) child[i] = nullptr;
+        Node()
+        {
+            for (int i = 0; i < 256; i++)
+                child[i] = nullptr;
             cnt = 0;
             exist = 0;
         }
     };
-
     Node node[maxn];
     Node *root;
     int cur = 0;
 
-    Node *newNode() {
+    Node *newNode()
+    {
         return &node[cur++];
     }
 
-    string lower(string s) {
-        for (char &c: s) {
-            if ('A' <= c && c <= 'Z') c = c - 'A' + 'a';
-        }
-        return s;
-    }
-
-    void add(string s) {
-        Node *p = root;
-        for (char c: s) {
-            if (p->child[c] == nullptr) p->child[c] = newNode();
-            p = p->child[c];
-            p->cnt++;
-        }
-        p->exist++;
-    }
-
-    void dfs(Node *p, string &cur) {
-        if (p->exist) {
+    void dfs(Node *p, string &cur)
+    {
+        if (p->exist)
+        {
             word.push_back(cur);
         }
-        for (int i = 0; i < 256; i++) {
-            if (p->child[i]) {
+        for (int i = 0; i < 256; i++)
+        {
+            if (p->child[i])
+            {
                 cur.push_back(char(i));
                 dfs(p->child[i], cur);
                 cur.pop_back();
             }
         }
     }
-    
-public:
 
-    void load_dictionary(string filename) {
-        ifstream f(filename);
-        f >> dt;
-        // input data to search dictionary
-        for (auto &item: dt) {
-            string id = lower(item["id"].get<string>());
-            string def = item["definition"].get<string>();
-            string term_val = item["term"].get<string>();
-            dict[id] = def;
-            term[id] = term_val;
-            add(id);
-        }   
+    void add(string s)
+    {
+        Node *p = root;
+        for (char c : s)
+        {
+            if (p->child[c] == nullptr)
+                p->child[c] = newNode();
+            p = p->child[c];
+            p->cnt++;
+        }
+        p->exist++;
     }
 
-    void Search(string prefix) {
+public:
+    unordered_map<string, WordData> dict;
+    void load_dictionary(string filename)
+    {
+        ifstream f(filename);
+        if (!f.is_open())
+        {
+            cerr << "Cannot open the dictionary file" << "\n";
+            abort();
+        }
+        f >> dt;
+        // Dictionary Data Reparation:
+        for (auto &item : dt)
+        {
+            // Temp vars
+            string id = lower(item["id"].get<string>());
+            string def = item["definition"].get<string>();
+            string def_vie = item["definition_vi"].get<string>();
+            bool in_rmap = item["in_roadmap"].get<bool>();
+            string term_val = item["term"].get<string>();
+            // Save to dict:
+            if (in_rmap && item["roadmap"].is_array())
+            {
+                vector<string> rmap = item["roadmap"].get<vector<string>>();
+                dict[id].roadmap = rmap;
+            }
+            dict[id].definition = def;
+            dict[id].definition_vi = def_vie;
+            dict[id].in_roadmap = in_rmap;
+            dict[id].term = term_val;
+            add(id);
+        }
+    }
+
+    void Search(string prefix)
+    {
         word.clear();
         prefix = lower(prefix);
         Node *p = root;
-        for (char c: prefix) {
-            if (p->child[c] == nullptr) return;
+        for (char c : prefix)
+        {
+            if (p->child[c] == nullptr)
+                return;
             p = p->child[c];
         }
-
-        if (p->exist) {
-            cout << "=====DEFINITION=====\n";
+        if (p->exist)
+        {
             string W = prefix;
-            cout << dict[W] << '\n';
+            cout << "=====DEFINITION_ENG=====\n";
+            cout << dict[W].definition << '\n';
+            cout << "=====DEFINITION_VIE=====\n";
+            cout << dict[W].definition_vi << '\n';
             dfs(p, prefix);
-            if (word.size() == 1) return;
+            if (word.size() == 1)
+                return;
             cout << "=====OTHER WORDS=====\n";
-            for (string s: word) if (s != W) cout << term[s] << '\n';
+            for (string s : word)
+                if (s != W)
+                    cout << dict[slugify(s)].term << '\n';
             return;
         }
-
         dfs(p, prefix);
-
-        if (word.size() == 0) cout << "NOT FOUND!!!";
-        else {
-            for (string s: word) cout << term[s] << '\n';
+        if (word.size() == 0)
+            cout << "NOT FOUND!!!";
+        else
+        {
+            for (string s : word)
+                cout << dict[slugify(s)].term << '\n';
         }
-        if (word.size() == 1) {
-            cout << "=====DEFINITION=====\n";
+        if (word.size() == 1)
+        {
             string W = word[0];
-            cout << dict[W] << '\n';
+            cout << "=====DEFINITION_ENG=====\n";
+            cout << dict[W].definition << '\n';
+            cout << "=====DEFINITION_VIE=====\n";
+            cout << dict[W].definition_vi << '\n';
         }
     }
-
-    SEARCH_DICTIONARY() {
+    SEARCH_DICTIONARY()
+    {
         root = newNode();
     }
 };
-
-//Taidebug
+// Taidebug

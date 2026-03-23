49fffa32bb73b34aed097793b8d563c496abc251
Taidebug <nguyenthanhtaicva7a8@gmail.com>
2026-03-23 22:25:39 +0700

    push file normalize

---
+++ C++/normalize.cpp
@@ -0,0 +1,45 @@
+#include<string>
+using namespace std;
+
+string lower(string s)
+{
+    for (char &c : s)
+    {
+        if ('A' <= c && c <= 'Z')
+            c = c - 'A' + 'a';
+    }
+    return s;
+}
+
+string upper(string s)
+{
+    for (char &c : s)
+    {
+        if ('a' <= c && c <= 'z')
+            c = c - 'a' + 'A';
+    }
+    return s;
+}
+
+string slugify(string s)
+{
+    int i = 0;
+    for (char &c : s)
+    {
+        if ('A' <= c && c <= 'Z')
+        {
+            c = c - 'A' + 'a';
+            s[i++] = c;
+        }
+        else if (('a' <= c && c <= 'z') || ('0' <= c && c <= '9'))
+        {
+            s[i++] = c;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    s = s.substr(0, i);
+    return s;
+}

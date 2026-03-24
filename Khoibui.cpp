d84bc3a7b09c79c13543c986454fa7ed71bce421
Bui Khoi <khoibui0992@gmail.com>
2026-03-24 16:00:56 +0700

    update Khoibui.cpp

--- Khoibui.cpp
+++ Khoibui.cpp
@@ -1,256 +1,214 @@
 #include <iostream>
-#include <string>
 #include <fstream>
 #include <vector>
+#include <string>
+#include <algorithm>
 #include <queue>
-#include <map>
-#include <windows.h>
+#include <stack>
+#include <unordered_map>
 #include "json.hpp"
-#define maxn 200007
-using namespace std;
+
 using json = nlohmann::json;
 
-// COLORS:
-const string RESET = "\033[0m";
-const string GREEN = "\033[1;32m"; // Highlight selected node
-// COLORS
-
+using namespace std;
 struct Node
 {
     string id;
     string term;
-    vector<string> next_nodes;
+    string classify;
+    vector<string> parent_nodes;
 };
 
-string lower(string s)
+map<string, vector<string>> nodes;
+map<string, string> ids;
+map<string, string> terms;
+map<string, vector<string>> parents;
+vector<Node> node_list;
+bool check_label = false;
+vector<string> Label_list;
+map<std::string, int> indegree;
+
+void build_graph(Node new_node)
 {
-    for (char &c : s)
+    for (auto v : new_node.parent_nodes)
     {
-        if ('A' <= c && c <= 'Z')
-            c = c - 'A' + 'a';
+        parents[new_node.id].push_back(v);
+        indegree[v] += 1;
     }
-    return s;
 }
-
-string upper(string s)
+void load_data(string roadmap, string knowledge)
 {
-    for (char &c : s)
+    // Open file
+    string file_input = roadmap;
+    ifstream file(file_input);
+    if (!file.is_open())
     {
-        if ('a' <= c && c <= 'z')
-            c = c - 'a' + 'A';
+        cout << "Cannot open roadmap files" << "\n";
+        abort();
     }
-    return s;
-}
-
-string slugify(string s)
-{
-    int i = 0;
-    for (char &c : s)
+    // Read file
+    json j_data;
+    file >> j_data;
+    // Create graph
+    for (auto &item : j_data)
     {
-        if ('A' <= c && c <= 'Z')
+        Node n;
+        n.id = item["id"];
+        n.term = item["term"];
+        n.classify = item["classify"];
+        if (n.classify == " ")
         {
-            c = c - 'A' + 'a';
-            s[i++] = c;
-        }
-        else if (('a' <= c && c <= 'z') || ('0' <= c && c <= '9'))
-        {
-            s[i++] = c;
-        }
-        else
-        {
-            continue;
-        }
-    }
-    s = s.substr(0, i);
-    return s;
-}
-
-class ROADMAP_CREATOR
-{
-private:
-    map<std::string, vector<std::string>> nodes, parents;
-    map<std::string, std::string> terms;
-    vector<Node> node_list;
-    map<std::string, int> indegree;
-    // Buiild Graph Structure:
-    void build_graph(Node new_node)
-    {
-        for (auto v : new_node.next_nodes)
-        {
-            nodes[new_node.id].push_back(v);
-            parents[v].push_back(new_node.id);
-            indegree[v] += 1;
-        }
-    }
-
-public:
-    void load_data(string roadmap)
-    {
-        // Open file
-        string file_input = roadmap;
-        ifstream file(file_input);
-        if (!file.is_open())
-        {
-            cout << "Cannot open the file" << "\n";
-            abort();
-        }
-        // Read file
-        json j_data;
-        file >> j_data;
-        // Create graph
-        for (auto &item : j_data)
-        {
-            Node n;
-            n.id = item["id"];
-            n.term = item["term"];
-            n.next_nodes = item["next_nodes"].get<vector<string>>();
-            build_graph(n);
-            terms[n.id] = n.term;
-            node_list.push_back(n);
-        }
-        file.close();
-    }
-
-    // BFS:
-    vector<string> RoadMap_from_start_point(string knowledge)
-    {
-        map<string, bool> check;
-        for (Node v : node_list)
-        {
-            check[v.id] = false;
-        }
-        vector<string> RoadMap;
-        RoadMap.push_back(upper(knowledge));
-        queue<string> q;
-        q.push(knowledge);
-        while (!q.empty())
-        {
-            string u = q.front();
-            q.pop();
-            // Go to it parent
-            for (string v : parents[u])
+            // cout << n.term << " " << knowledge << endl;
+            if (knowledge == n.term)
             {
-                // If this knowledge already in RoadMap -> skip
-                if (check[v])
-                    continue;
-                RoadMap.push_back(upper(terms[v]));
-                q.push(v);
-                check[v] = true;
+                check_label = true;
             }
         }
-        reverse(RoadMap.begin(), RoadMap.end());
-        return RoadMap;
+        n.parent_nodes = item["link"].get<vector<string>>();
+        build_graph(n);
+        ids[n.term] = n.id;
+        terms[n.id] = n.term;
+        node_list.push_back(n);
+        if (n.classify == ids[knowledge])
+            Label_list.push_back(n.id);
     }
+    file.close();
+}
 
-    // Topological Sort:
-    vector<string> Roadmap_fullcourse(string knowledge)
+// add node to graph
+void add_node(Node new_node)
+{
+    for (auto v : new_node.parent_nodes)
     {
-        queue<string> listSource;
-        vector<string> res;
-        map<string, int> temp_indegree = indegree;
-        for (auto node : node_list)
-        {
-            if (!temp_indegree[node.id])
-                listSource.push(node.id);
-        }
-        while (!listSource.empty())
-        {
-            string u = listSource.front();
-            listSource.pop();
-            res.push_back(upper(terms[u]));
-            for (auto child : nodes[u])
-            {
-                temp_indegree[child]--;
-                if (!temp_indegree[child])
-                    listSource.push(child);
-            }
-        }
-        return res;
+        parents[new_node.id].push_back(v);
     }
-};
+}
+
+// check if id is label
+
+// bfs to retrieval
+vector<string> get_RoadMap_Knowledge(string knowledge)
+{
+    map<string, bool> visited;
+    for (Node v : node_list)
+    {
+        visited[v.id] = false;
+    }
+    vector<string> RoadMap;
+    RoadMap.push_back(terms[knowledge]);
+    queue<string> q;
+    q.push(knowledge);
+    while (!q.empty())
+    {
+        string u = q.front();
+        q.pop();
+        // go to it parent
+        for (string v : parents[u])
+        {
+            //  if this knowledge already in RoadMap -> skip
+            if (visited[v])
+                continue;
+            RoadMap.push_back(terms[v]);
+            q.push(v);
+            visited[v] = true;
+        }
+    }
+    RoadMap.pop_back();
+    RoadMap.push_back("Competitive Programming");
+    reverse(RoadMap.begin(), RoadMap.end());
+    return RoadMap;
+}
+
+vector<string> get_RoadMap_Label(string Label)
+{
+    map<string, bool> visited;
+    queue<string> q;
+    map<string, int> inDegree;
+    vector<string> topo_list;
+    for (Node i : node_list)
+    {
+        visited[i.id] = false;
+    }
+    for (string i : Label_list)
+    {
+        q.push(i);
+        inDegree[i] = 0;
+    }
+    while (!q.empty())
+    {
+        string u = q.front();
+        q.pop();
+        topo_list.push_back(u);
+        for (string v : parents[u])
+        {
+            if (visited[v])
+            {
+                continue;
+            }
+            visited[v] = true;
+            inDegree[v] = 0;
+            q.push(v);
+        }
+    }
+    for (string u : topo_list)
+    {
+        for (string v : parents[u])
+        {
+            inDegree[v]++;
+        }
+    }
+    queue<string> listSource;
+    vector<string> res;
+    map<string, int> temp_indegree = inDegree;
+    for (auto node : topo_list)
+    {
+        if (!temp_indegree[node])
+            listSource.push(node);
+    }
+    while (!listSource.empty())
+    {
+        string u = listSource.front();
+        listSource.pop();
+        res.push_back(terms[u]);
+        for (auto child : parents[u])
+        {
+            temp_indegree[child]--;
+            if (!temp_indegree[child])
+                listSource.push(child);
+        }
+    }
+    res.pop_back();
+    res.push_back("Competitive Programming");
+    reverse(res.begin(), res.end());
+    return res;
+}
 
 int main()
 {
-#ifdef _WIN32
-    SetConsoleOutputCP(65001);
-#endif
-    while (true)
+    string knowledge;
+    cout << "Enter the knowledge you want to learn: ";
+    getline(cin, knowledge);
+    load_data("DSA-Project/Dataset/Dataset_JSONfiles/roadmap.json", knowledge);
+    cout << "Suggested learning path: \n";
+    cout << "---------------\n\n";
+    vector<string> Roadmap;
+    if (check_label)
     {
-        ROADMAP_CREATOR roadmap;
-        string user_input;
-        cout << "Enter your categories: ";
-        getline(cin, user_input);
-        if (slugify(user_input) == "end")
+        Roadmap = get_RoadMap_Label(ids[knowledge]);
+    }
+    else
+    {
+        Roadmap = get_RoadMap_Knowledge(ids[knowledge]);
+    }
+    for (auto v : Roadmap)
+    {
+        cout << v;
+        if (v != Roadmap.back())
         {
-            cout << "Thanks for using our app. LOVE LOVE <3 <3" << "\n";
-            break;
-        }
-        user_input = "../Dataset/Dataset_JSONfiles/Roadmap/" + user_input + ".json";
-        roadmap.load_data(user_input);
-        string knowledge;
-        cout << "Enter the knowledge you want to learn: ";
-        getline(cin, knowledge);
-        cout << "Would you like a short or a long roadmap ? [1 for short || 2 for long]:";
-        int choice;
-        cin >> choice;
-        cin.ignore();
-        int choice;
-        while (true)
-        {
-            cout << "Would you like a short or a long roadmap ? [1 for short || 2 for long]: ";
-            if (cin >> choice && (choice == 1 || choice == 2))
-            {
-                cin.ignore();
-                break;
-            }
-            cout << "I HATE YOU! Enter 1 or 2: ";
-            cin.clear();
-            cin.ignore(1000, '\n');
-        }
-        if (choice == 1)
-        {
-            vector<string> roadmap_print = roadmap.RoadMap_from_start_point(slugify(knowledge));
-            for (auto v : roadmap_print)
-            {
-                if (v != roadmap_print.back())
-                {
-                    cout << v << " -> ";
-                }
-                else
-                {
-                    cout << GREEN << "[*" << v << "]" << RESET << "\n";
-                }
-            }
-        }
-        else if (choice == 2)
-        {
-            vector<string> roadmap_print = roadmap.Roadmap_fullcourse(slugify(knowledge));
-            for (size_t i = 0; i < roadmap_print.size(); i++)
-            {
-                string v = roadmap_print[i];
-                if (slugify(v) == slugify(knowledge))
-                {
-                    cout << GREEN << "[*" << v << "]" << RESET;
-                }
-                else
-                {
-                    cout << v;
-                }
-                if (i != roadmap_print.size() - 1)
-                {
-                    cout << " -> ";
-                }
-                else
-                {
-                    cout << "\n";
-                }
-            }
-        }
-        else
-        {
-            cout << "I HATE YOU" << endl;
-            abort();
+            cout << " -> ";
         }
     }
+    cout << "\n\n";
+    cout << "---------------";
     return 0;
 }
